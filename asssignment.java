1b12bb44a7c54a7e64145dae25e27081e7c2648f
AgilanMuthusamy <91741846+AgilanMuthusamy@users.noreply.github.com>
2021-10-02 23:33:57 +0530

    Add files via upload

---
+++ asssignment.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class asssignment {
+
+	public static void main(String[] args) {
+   
+		int number = -40;
+		if (number<0) {
+			System.out.println(number+ "is converted to"+ -number);
+			
+		}
+		int number1 = 35;
+		if (number1>0) {
+			System.out.println("The number is positive");
+			
+		}else if (number1<0) {
+			System.out.println("The number is negative");
+			
+		}else {
+			System.out.println("The number is neither positive nor negative");
+		}
+	}
+
+}
